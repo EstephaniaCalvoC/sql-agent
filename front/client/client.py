87e862ced025b449f49b491ae095848c321fd42b
Estephania Calvo <estephania.calvo.c@gmail.com>
2024-10-22 08:56:24 -0500

    Create client in the front

---
+++ front/client/client.py
@@ -0,0 +1,28 @@
+import uuid
+import requests
+
+from common import logger
+
+
+class SQLAgentClient:
+    def __init__(self, base_url: str="http://localhost:8000") -> None:
+        logger.info("Start Client")
+        self.base_url = base_url
+
+    def query(self, question: str) -> str:
+        thread_id = str(uuid.uuid4())
+        logger.info(f"Ask question: {question} with thread_id: {thread_id}")
+        response = requests.post(f"{self.base_url}/query", json={"question": question, "thread_id": thread_id})
+        raw_response = response.json()
+        
+        if response.status_code != 200:
+            logger.error(f"Error from server: {raw_response}")
+            raise Exception(f"Ups, something went wrong: {raw_response}")
+        
+        return {"answer": raw_response.get('answer'), "error_message": raw_response.get('error_message')}
+    
+    
+if __name__ == "__main__":
+    # Test the client
+    client = SQLAgentClient()
+    print(client.query("How many albums does Led Zeppelin have?"))
---
+++ front/client/common.py
@@ -0,0 +1,15 @@
+import logging
+from dotenv import load_dotenv
+
+
+load_dotenv(override=True)
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
